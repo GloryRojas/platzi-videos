9ad881ed67c3bb9090285ba704eb10ca169bdcb1
gloryrojas <glory.rojas>
2021-03-01 18:15:59 -0500

    feature: adding redux to login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,68 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+import { logOutRequest } from '../actions';
 import '../assets/styles/components/_header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <Link to='/'>
-            Cuenta
-          </Link>
-        </li>
-        <li>
-          <Link to='/login'>
-            Cerrar Sesión
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props) => {
 
-export default Header;
+  const { user } = props;
+
+  const hasUser = Object.keys(user).length;
+
+  const handleLogOut = () => {
+    props.logOutRequest({});
+    props.history.push('/login');
+  };
+
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          { hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt='User' />}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          { hasUser ? (
+            <li>
+              <Link to='/'>
+                {user.name}
+              </Link>
+            </li>
+          ) : null}
+          { hasUser ? (
+            <li onClick={handleLogOut}>
+              Cerrar Sesión
+            </li>
+          ) : (
+            <li>
+              <Link to='/login'>
+                Cerrar Sesión
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logOutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,45 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginRequest } from '../actions';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/_login.scss';
-import { Link } from 'react-router-dom';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1' value='first_checkbox' />
-            Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='' />
-          {' '}
-          Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='' />
-          {' '}
-          Inicia sesión con Twitter
-        </div>
+const Login = (props) => {
+  const [form, setForm] = useState({});
+
+  const handleInput = (e) => {
+    const { target: { value, name } } = e;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+  };
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form onSubmit={handleSubmit} className='login__container--form'>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Iniciar sesión</button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' id='cbox1' value='first_checkbox' />
+              Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='' />
+            {' '}
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} alt='' />
+            {' '}
+            Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta
+          <Link to='/register'>
+            Regístrate
+          </Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta
-        <Link to='/register'>
-          Regístrate
-        </Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
