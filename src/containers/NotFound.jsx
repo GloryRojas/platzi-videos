0d5d78098835d9e5664a325bd1cddbcfed8e83cf
gloryrojas <glory.rojas>
2021-03-01 15:10:46 -0500

    feature: adding notFound component

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/_notFound.scss';
+
+const NotFound = () => (
+  <section className='not-found'>
+    <h2 className='not-found__title'>404</h2>
+    <h3>Página no encontrada</h3>
+  </section>
+);
+
+export default NotFound;
