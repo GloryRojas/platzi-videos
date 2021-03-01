74bb7cfa901d423088f25172db6de24fe67c1651
gloryrojas <glory.rojas>
2021-03-01 12:36:05 -0500

    feature: using hook to fetch information

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,41 @@ import Categories from './components/Categories';
 import Carousel from './components/Carousel';
 import CarouselItem from './components/CarouselItem';
 import Footer from './components/Footer';
+import useInitialState from './hooks/useInitialState';
 
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Search />
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const API = 'http://localhost:3000/initialState';
 
-    <Categories title='Tu lista'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const initialState = useInitialState(API);
 
-    <Categories title='Nuestra lista'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+  return (
+    <div className='app'>
+      <Header />
+      <Search />
+      { (initialState.mylist && initialState.mylist.length) && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      { (initialState.originals && initialState.originals.length) && (
+        <Categories title='Originales'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      { (initialState.trends && initialState.trends.length) && (
+        <Categories title='Tendencias'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
