8e8581e130ef0635c8603ea7c0a5b1979c00ef2e
gloryrojas <glory.rojas>
2021-03-01 15:01:41 -0500

    feature: adding login component and router for this component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from './components/Header';
-import Search from './components/Search';
-import Categories from './components/Categories';
-import Carousel from './components/Carousel';
-import CarouselItem from './components/CarouselItem';
-import Footer from './components/Footer';
-import useInitialState from './hooks/useInitialState';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return (
@@ -48,4 +48,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
